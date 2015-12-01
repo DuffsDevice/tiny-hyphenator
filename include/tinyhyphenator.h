71eb17fdf2e26976a46db4198205ca968ae11a04
Jakob Riedle <13666193+DuffsDevice@users.noreply.github.com>
2015-12-01 13:34:27 +0000

    Adjusted Versioning

--- include/tinyhyphenator.h
+++ include/tinyhyphenator.h
@@ -1,6 +1,6 @@
 ﻿// Copyright (c) 2012-2014 Jakob Riedle (DuffsDevice)
 // 
-// Version 1.0
+// Version 1.2
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
